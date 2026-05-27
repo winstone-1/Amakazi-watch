9ac8eaef4e9f83eb33d07adb4ab49740bb519502
winstone-1 <winstonemuna404@gmail.com>
2026-05-27 11:00:25 +0300

    feat(users): custom User model with RBAC roles

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'role', 'is_staff']
+    list_filter  = ['role', 'is_staff']
+    fieldsets    = BaseUserAdmin.fieldsets + (
+        ('AmakaziWatch Role', {'fields': ('role',)}),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN     = 'admin',     'Admin'
+        ORG_STAFF = 'org_staff', 'Organisation Staff'
+        PUBLIC    = 'public',    'Public'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.PUBLIC
+    )
+
+    def is_org_staff(self):
+        return self.role == self.Role.ORG_STAFF
+
+    def is_platform_admin(self):
+        return self.role == self.Role.ADMIN or self.is_superuser
+
+    def __str__(self):
+        return self.username

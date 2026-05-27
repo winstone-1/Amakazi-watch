35c1e3146742c3b460e9abc32a3cd913a0bf8fa9
winstone-1 <winstonemuna404@gmail.com>
2026-05-27 11:01:39 +0300

    feat(content): EducationContent and Quiz models with admin

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import EducationContent, Quiz
 
-# Register your models here.
+@admin.register(EducationContent)
+class EducationContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'format', 'topic', 'organisation', 'approved', 'created_at']
+    list_filter  = ['format', 'topic', 'approved']
+    actions      = ['approve_content']
+
+    @admin.action(description='Approve selected content')
+    def approve_content(self, request, queryset):
+        queryset.update(approved=True)
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['title', 'topic', 'organisation', 'approved', 'completion_count']
+    list_filter  = ['approved', 'topic']

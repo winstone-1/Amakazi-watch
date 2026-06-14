45a5c9962afe338a7cb7f556d991d311032b7ffa
winstone-1 <winstonemuna404@gmail.com>
2026-06-15 00:19:31 +0300

    feat(scorecard): Add county-level performance tracking and rankings

--- county_scorecard/admin.py
+++ county_scorecard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CountyScorecard, ScorecardHistory
 
-# Register your models here.
+@admin.register(CountyScorecard)
+class ScorecardAdmin(admin.ModelAdmin):
+    list_display = ('county', 'overall_score', 'rank', 'updated_at')
+    
+@admin.register(ScorecardHistory)
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('county', 'snapshot_date')

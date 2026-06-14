c4a99823048335059477f6b1127ce5c471ae243b
winstone-1 <winstonemuna404@gmail.com>
2026-06-15 00:19:31 +0300

    feat(workshops): Add virtual workshops with registration and feedback

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Workshop, WorkshopAttendance
 
-# Register your models here.
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'workshop_type', 'is_live', 'scheduled_start', 'scheduled_end')
+    list_filter = ('workshop_type', 'is_live')
+
+@admin.register(WorkshopAttendance)
+class WorkshopAttendanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'workshop', 'attended_at', 'feedback_rating')

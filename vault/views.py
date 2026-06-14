7ce33f7c0758ea372d99fadb3fbc01950ea0bf27
winstone-1 <winstonemuna404@gmail.com>
2026-06-15 00:19:30 +0300

    feat(vault): Add encrypted evidence storage with court-ready metadata

--- vault/views.py
+++ vault/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.http import FileResponse
+from .models import EvidenceDocument
+from .serializers import EvidenceDocumentSerializer
 
-# Create your views here.
+class EvidenceDocumentViewSet(viewsets.ModelViewSet):
+    serializer_class = EvidenceDocumentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return EvidenceDocument.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    @action(detail=True, methods=['get'])
+    def download(self, request, pk=None):
+        doc = self.get_object()
+        return FileResponse(doc.file, as_attachment=True)

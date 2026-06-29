f3166b91ad3b2fb9dbb733e2055f8c503a540afa
winstone-1 <winstonemuna404@gmail.com>
2026-06-29 10:30:11 +0300

    Fix: Add whitenoise and finalize backend deployment

--- amakaziwatch/wsgi.py
+++ amakaziwatch/wsgi.py
@@ -1,15 +1,14 @@
 """
 WSGI config for amakaziwatch project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Add apps directory to Python path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'apps'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amakaziwatch.settings')
 

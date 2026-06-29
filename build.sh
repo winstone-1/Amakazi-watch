c0ac0ddfa9436d0dedceb266dbe5846d8be9fd80
winstone-1 <winstonemuna404@gmail.com>
2026-06-29 08:09:20 +0300

    Fix migration issue

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -o errexit
 
-# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --noinput
+
+# Remove problematic migration if exists
+if [ -f "reports/migrations/0004_fix_report_models.py" ]; then
+    rm reports/migrations/0004_fix_report_models.py
+fi
 
 # Run migrations
 python manage.py migrate
